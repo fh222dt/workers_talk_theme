3b6c69238b3ce8a63ad8b3cc94f9a98e39d2a49e
fh222dt <fh222dt@student.lnu.se>
2016-05-19 14:23:47 +0200

    fiddleing with benefits

--- partials/employer-benefits.php
+++ partials/employer-benefits.php
@@ -9,7 +9,51 @@
 
 
 <?php
-function benefits() {
+//function benefits() {
 	//hämta alla förmåner per employer
-	//skriv ut 
+	//skriv ut
+	$benefits = do_benefits();
+	//print_r($benefits);
+
+	$output = '';
+	foreach($benefits as $benefit=>$category) {
+		$output.=
+		'<div class="employer-benefits">
+			<div class="benefit-category">
+
+				<h6>'. $category .'</h6>';
+
+				//$confirmed 	TODO
+				if($benefit == $confirmed) {
+					$output .= '<p class="benefit-confirmed">'. $benefit.'</p>';
+				}
+				else {
+					$output .= '<p class="benefit-not-confirmed">'. $benefit .'</p>';
+				}
+			$output .=
+			'</div>
+		</div>';
+	}
+	echo $output;
+
+//}
+
+function do_benefits() {
+	//hämtar alla förmåner som finns, ska ju bara ha dem till detta företag
+    $raw_benefits = get_terms(array(
+        'taxonomy' => 'benefit',
+        'orderby' => 'meta_value',
+        'hide_empty' => false,
+        //'meta_value' => 'Försäkringar & Hälsa',        //search by category name
+    ));
+	$benefits_from_answers = null;
+    $benefits = [];
+    foreach ($benefits_from_answers as &$benefit) {
+        $id = $benefit['anwser'];		//en massa term_id:n
+		$category = get_term_meta($benefit->term_id, 'benefit-category', $single = true);
+        //array_push($benefits, $name=>$category);
+        $benefits[$name] = $category;
+    }
+
+    return $benefits;
 }
